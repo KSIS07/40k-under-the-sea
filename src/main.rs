524ec7d11bd0a9b27cbeb9c837fd932cc0413383
KSIS07 <darm.draussen_0t@icloud.com>
2024-03-17 22:14:00 +0100

    new file:   src/images/u-boat/sonar.png 	renamed:    src/images/u-boat-detailed.png -> src/images/u-boat/u-boat-detailed.png 	renamed:    src/images/u-boat.png -> src/images/u-boat/u-boat.png 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("40k-under-sea")]
 async fn main() {
     const WATERBLUE: macroquad::color::Color = Color::new(0.0, 0.0, 0.2, 1.0);
-    const ZOOM: f32 = 5.0;
+    const ZOOM: f32 = 4.0;
     const SPEED: f32 = 1.0;
 
     const TOP_DECK: f32 = 112.0 * ZOOM;
@@ -12,11 +12,13 @@ async fn main() {
     let mut on_ladder: bool = false;
 
     let ladder_hitbox: Vec<f32> = vec![710.0 * ZOOM, 731.0 * ZOOM];
+    let sonar_hitbox: Vec<f32> = vec![565.0 * ZOOM, 587.0 * ZOOM];
+    let steer_hitbox: Vec<f32> = vec![453.0 * ZOOM, 468.0 * ZOOM];
 
     let top_deck_hitbox: Vec<f32> = vec![261.0 * ZOOM, 731.0 * ZOOM];
     let bottom_deck_hitbox: Vec<f32> = vec![218.0 * ZOOM, 771.0 * ZOOM];
 
-    let background_image: Texture2D = load_texture("images/u-boat-detailed.png").await.unwrap();
+    let background_image: Texture2D = load_texture("images/u-boat/u-boat-detailed.png").await.unwrap();
     background_image.set_filter(FilterMode::Nearest);
     let player_image: Texture2D = load_texture("images/player/player.png").await.unwrap();
     player_image.set_filter(FilterMode::Nearest);
@@ -85,6 +87,9 @@ async fn main() {
                     on_ladder = true;
                 }
             }
+            if player_x >= sonar_hitbox[0] && player_x <= sonar_hitbox[1] && player_y == TOP_DECK {
+                sonar();
+            }
         }
 
         if on_ladder {
@@ -107,7 +112,39 @@ async fn main() {
             dest_size: Some(vec2(player_image.width() * ZOOM, player_image.height() * ZOOM)),
             ..Default::default()
         },);
+        
 
+        next_frame().await
+    }
+}
+
+async fn sonar() {
+    let mut zoom: f32 = 1.0;
+
+    const BACKGROUND_GRAY: macroquad::color::Color = Color::new(122.0, 134.0, 153.0, 1.0);
+
+    let sonar_image: Texture2D = load_texture("images/u-boat/sonar.png").await.unwrap();
+    sonar_image.set_filter(FilterMode::Nearest);
+
+    let mut camera_x: f32;
+    let mut camera_y: f32;
+    let mut camera: Camera2D;
+
+    loop {
+
+        camera_x = sonar_image.width() / 2.0 * zoom - screen_width() / 2.0;
+        camera_y = sonar_image.height() / 2.0 * zoom - screen_height() / 2.0;
+        camera = Camera2D::from_display_rect(Rect::new(camera_x, camera_y + screen_height(), screen_width(), screen_height() * -1.0));
+        set_camera(&camera);
+
+        clear_background(BACKGROUND_GRAY);
+
+        draw_texture_ex(&sonar_image, 0.0, 0.0, WHITE, DrawTextureParams {
+            dest_size: Some(vec2(sonar_image.width() * zoom, sonar_image.height() * zoom)),
+            ..Default::default()
+        },);
+
+        
         next_frame().await
     }
 }
